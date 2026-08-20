cb2b60441d7454bfbb3fc1efd22b6fcbf97f19f0
licethnperdomo-tecnología <licetnperdomo@gmail.com>
2026-08-20 07:37:35 -0500

    routes auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import { iniciarSesion } from '../controllers/auth.js';
+
+import {
+    iniciarSesion,
+    solicitarRecuperacion,
+    restablecerContrasena
+} from '../controllers/auth.js';
 
 const router = Router();
 
 // Iniciar sesión
 router.post('/login', iniciarSesion);
+
+// Solicitar código de recuperación
+router.post('/recuperar', solicitarRecuperacion);
+
+// Restablecer contraseña
+router.post('/restablecer', restablecerContrasena);
 
 export default router;
