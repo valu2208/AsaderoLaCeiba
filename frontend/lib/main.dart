d52297d896f32fe5afa850178923787fa8dfce5b
valor2208 <danna08vsh@gmail.com>
2026-09-25 10:59:41 -0500

    Actualizar main del frontend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:asadero/core/colores.dart';
-import 'package:asadero/pantallas/pantalla_inicio.dart';
 import 'package:flutter/material.dart';
+import 'package:asadero/pantallas/bienvenida.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.fondoComponentesSeleccionado,
-          foregroundColor: Colors.white,
-          title: Text("Calcular IMC"),
-        ),
-        backgroundColor: AppColors.accent,
-        body: PantallaInicio(),
-      ),
+      home: const Bienvenida(),
     );
   }
 }
